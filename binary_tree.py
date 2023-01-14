78634100d54d0344f31aabbef107b0fdab430961
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-14 09:42:01 +0800

    Checks if left subtree has value

--- binary_tree.py
+++ binary_tree.py
@@ -11,4 +11,7 @@ class BinarySearchTreeNode:
 
     # left subtree
     if data < self.data:
-      self.left = BinarySearchTreeNode(data)
+      if self.data:
+        pass
+      else:
+        self.left = BinarySearchTreeNode(data)
