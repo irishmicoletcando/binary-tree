f45b98e867561ce34eb829da520d529810e60379
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-14 11:46:44 +0800

    Add element in base node to elements list based on post order traversal

--- binary_tree_part1_exercises.py
+++ binary_tree_part1_exercises.py
@@ -96,6 +96,7 @@ class BinarySearchTreeNode:
       elements += self.left.post_order_traversal()
     if self.right:
       elements += self.right.post_order_traversal()
+    elements.append(self.data)
     return elements
 
     
