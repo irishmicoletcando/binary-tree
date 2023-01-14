1fd43e7b19b46c6aff18689e9bbbcb591797c6d2
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-14 14:41:55 +0800

    Returns none if value is not found in left or right subtree

--- binary_tree.py
+++ binary_tree.py
@@ -79,6 +79,13 @@ class BinarySearchTreeNode:
     elif val > self.data:
       if self.right:
         self.right = self.right.delete(val)
+    else:
+      if self.left is None and self.right is None:
+          return None
+      elif self.left is None:
+          return self.right
+      elif self.right is None:
+          return self.left
       
 def build_tree(elements):
   root = BinarySearchTreeNode(elements[0])
