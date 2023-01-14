cd0360fa3762aca90ca7cfd10ed8f1bee945ce16
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-14 15:11:49 +0800

    Remove unnecessary print

--- binary_tree_part2_exercise.py
+++ binary_tree_part2_exercise.py
@@ -107,5 +107,3 @@ def build_tree(elements):
 if __name__ == '__main__':
   numbers = [17, 4, 1, 20, 9, 23, 18, 34]
   numbers_tree = build_tree(numbers)
-  print(numbers_tree.in_order_traversal())
-  print(numbers_tree.search(20))
