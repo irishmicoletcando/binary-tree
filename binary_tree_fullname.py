0daf5e4d26437312c9cc1c58150dd95aa297ef1f
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-14 12:38:02 +0800

    Repeat code in left subtree to right subtree

--- binary_tree_fullname.py
+++ binary_tree_fullname.py
@@ -14,3 +14,8 @@ class BinarySearchTreeNode:
           self.left.add_child(data)
         else:
           self.left = BinarySearchTreeNode(data)
+      else:
+        if self.right:
+          self.right.add_child(data)
+        else:
+          self.right = BinarySearchTreeNode(data)
